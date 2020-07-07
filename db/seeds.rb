5ea950fdfc552cc52f8c3e7ee4446f4b25c8e7bd
Wolhaa <wolhaa@yahoo.com>
2020-07-07 20:12:07 +0300

    fix_lesson_4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create!([
+users = User.create([
   { login: 'user1', password: 'password1' },
   { login: 'user2', password: 'password2' }
 ])
 
-categories = Category.create!([
+categories = Category.create([
   { title: 'Математика' },
   { title: 'Русский язык' }
 ])
 
-tests = Test.create!([
+tests = Test.create([
   { title: 'Вычитание', level: 1, category_id: 1 },
   { title: 'Сложение', category_id: 1 },
   { title: 'Правописание', level: 2, category_id: 2 }
 ])
 
-questions = Question.create!([
+questions = Question.create([
   { body: '5+5?', test_id: 1 },
   { body: '4-2?', test_id: 2 },
   { body: 'После Ж и Ш пишется И или Ы?', test_id: 3 }
 ])
 
-answers = Answer.create!([
+answers = Answer.create([
   { body: '10', correct: true, question_id: 1 },
   { body: '2', correct: true, question_id: 2 },
   { body: 'И', correct: true, question_id: 3 }
