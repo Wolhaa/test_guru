90dbe78671ce773f1ade257820bc557fe14d2324
Wolhaa <wolhaa@yahoo.com>
2020-07-19 17:47:05 +0300

    fix_lesson_6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :correct, -> { where(correct: true)}
-
   validates :body, presence: true
   validate :validate_max_answers, on: :create
+
+  scope :correct, -> { where(correct: true)}
 
   private
 
   def validate_max_answers
-    errors.add(:body) if question.answers.size > 4
+    errors.add(:body) if question.answers.count >= 4
   end
 end
