ba1f79292bf19f54545e9ed4572d26b832d15a44
Wolhaa <wolhaa@yahoo.com>
2020-08-02 20:27:04 +0300

    fix_lesson_8

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy edit update]
-  before_action :find_test, only: %i[index create new]
+  before_action :find_test, only: %i[create new]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    @questions = @test.questions
-  end
 
   def new
     @question = @test.questions.new
@@ -17,7 +14,7 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to action: 'index', id: @question.test_id
+      redirect_to @question
     else
       render :new
     end
@@ -25,6 +22,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
+    redirect_to test_questions_path(@question.test_id)
   end
 
   def update
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
-  def question_header
-    if @question.new_record?
-      "Creating a new question for test #{@question.test.title}"
+  def question_header(question)
+    if question.new_record?
+      "Creating a new question for test #{question.test.title}"
     else
-      "Edit question for test #{@question.test.title}"
+      "Edit question for test #{question.test.title}"
     end
   end
 end
