0ae166037de975364556713421ddeefa8b04fde2
Wolhaa <wolhaa@yahoo.com>
2020-08-09 18:16:09 +0300

    lesson_9

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,6 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :set_user, only: :start
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
@@ -36,17 +37,26 @@ class TestsController < ApplicationController
     redirect_to tests_path
   end
 
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:id])
   end
 
+  def set_user
+    @user = User.first
+  end
+
   def test_params
-    params.require(:test).permit(:category_id, :title, :author_id)
+    params.require(:test).permit(:category_id, :title, :user_id)
   end
 
   def rescue_with_test_not_found
-    render plain: 'Ресурс не найден.'
+    render plain: 'Resource is not found.'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_many :tests_user, dependent: :destroy
-  has_many :tests, through: :tests_user
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'user_id', dependent: :destroy
 
   def tests_by_level(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
