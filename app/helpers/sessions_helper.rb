fb49d1d3d63eabc8e4ff15a82320359a2fbd75d1
Wolhaa <wolhaa@yahoo.com>
2020-08-17 19:13:47 +0300

    fix_2_lesson_10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
   def flash_message(type)
-  content_tag :p, flash[type], class: "flash #{type}" if flash[type]
+    content_tag :p, flash[type], class: "flash #{type}" if flash[type]
   end
 end
