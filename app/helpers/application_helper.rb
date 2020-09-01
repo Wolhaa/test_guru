7d0033800605c7ea31002492679b139a26aa27a7
Wolhaa <wolhaa@yahoo.com>
2020-09-01 21:20:35 +0300

    lesson_13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
+  ALERTS = { alert: 'danger', notice: 'primary' }.freeze
+
   def current_year
     Time.current.year
   end
 
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}"
+  end
+
+
+  def alert_manager(key)
+    ALERTS[key.to_sym] || key
   end
 end
