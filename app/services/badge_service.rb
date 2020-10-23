52d7a03e6df84ac0ecf15bec14a1639184695612
Wolhaa <wolhaa@yahoo.com>
2020-10-23 10:40:53 +0300

    fix_3_lesson_18_badge

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -1,15 +1,6 @@
 class BadgeService
   BADGES_TYPE = %i[all_category_tests first_attempt all_level_tests].freeze
 
-  RULE_VALUES = [
-    '0-easy',
-    '1-elementary',
-    '2-advanced',
-    '3-hard',
-    '1',
-    'Математика',
-    'Русский язык'].freeze
-
   def initialize(test_passage)
     @test_passage = test_passage
     @user = test_passage.user
@@ -17,38 +8,48 @@ class BadgeService
 
   def give_badges
     Badge.all.select { |badge| send("#{badge.rule}_award", badge) }
-    byebug
   end
 
   private
 
   def all_category_tests_award(badge)
-    badge if badge.rule_value == @test_passage.test.category.title && all_category_tests?(badge.rule_value)
+    return false unless @test_passage.passed = false || badge.rule_value == @test_passage.test.category.title
+    badge.rule_value == @test_passage.test.category.title && all_category_tests?(badge.rule_value)
   end
 
   def first_attempt_award(badge)
-    badge if first_attempt?(badge.rule_value)
+    return false unless @test_passage.passed = false
+    first_attempt?
   end
 
   def all_level_tests_award(badge)
-    return false unless @test_passage.passed = false
-    badge if all_level_tests?(badge.rule_value) && badge.rule_value.to_i == @test_passage.test.level
+    return false unless @test_passage.passed = false || badge.rule_value.to_i == @test_passage.test.level
+    badge.rule_value.to_i == @test_passage.test.level && all_level_tests?(badge.rule_value)
   end
 
-  def all_category_tests?(category)
-    passed_tests_by_category = @user.tests.where('test_passages.passed = ?', true).by_category(category)
-    uniq_passed_tests_by_category = passed_tests_by_category.uniq { |test| test[:id] }
-    byebug
+  def all_category_tests?(badge)
+    last_badge_time = UsersBadge.where(badge: badge).order(created_at: :desc).first&.created_at
+    passed_tests_by_category = @user.tests.where('test_passages.passed = ?', true).by_category(badge.rule_value)
+    if last_badge_time.present?
+      uniq_passed_tests_by_category = passed_tests_by_category.where('test_passages.created_at > ?', last_badge_time).uniq { |test| test[:id] }
+    else
+      uniq_passed_tests_by_category = passed_tests_by_category.uniq { |test| test[:id] }
+    end
     Test.by_category(category).count == uniq_passed_tests_by_category.count
   end
 
-  def all_level_tests?(level)
-    passed_tests_by_level = @user.tests.where('test_passages.passed = ?', true).by_level(level.to_i)
-    uniq_passed_tests_by_level = passed_tests_by_level.uniq { |test| test[:id] }
+  def all_level_tests?(badge)
+    last_badge_time = UsersBadge.where(badge: badge).order(created_at: :desc).first&.created_at
+    passed_tests_by_level = @user.tests.where('test_passages.passed = ?', true).by_level(badge.rule_value.to_i)
+    if last_badge_time.present?
+      uniq_passed_tests_by_level = passed_tests_by_level.where('test_passages.created_at > ?', last_badge_time).uniq { |test| test[:id] }
+    else
+      uniq_passed_tests_by_level = passed_tests_by_level.uniq { |test| test[:id] }
+    end
     Test.by_level(level.to_i).count == uniq_passed_tests_by_level.count
   end
 
-  def first_attempt?(attempts)
-    @user.tests.where(id: @test_passage.test_id).count == attempts.to_i
+  def first_attempt?
+    @user.tests.where(id: @test_passage.test_id).count == '1'
   end
 end
